95fdee4b5eaaba2c3a4ef906100cdf612a60e10d
SEMANUR BEYAZYÜZ <s240307007@ankarabilim.edu.tr>
2025-12-23 16:30:14 +0300

    Update database URI and secret key in config.py

--- config.py
+++ config.py
@@ -1,11 +1,8 @@
-import os
-
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
 class Config:
-    SECRET_KEY = "secret123"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "instance", "app.db")
+    SQLALCHEMY_DATABASE_URI = "sqlite:////tmp/app.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = "super-secret-key"
+
 
 
 
